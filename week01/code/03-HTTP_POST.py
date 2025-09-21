293a6229abadca83733cea1ff61a26d96bdca76f
sunyx799 <1326170446@qq.com>
2025-09-21 14:03:25 +0800

    update 3.12版本autogen-ext依赖问题

--- week01/code/03-HTTP_POST.py
+++ week01/code/03-HTTP_POST.py
@@ -6,7 +6,7 @@ from dotenv import load_dotenv
 # 加载环境变量
 load_dotenv()
 
-conn = http.client.HTTPSConnection("api.gpt.ge")
+conn = http.client.HTTPSConnection("aihubmix.com")
 payload = json.dumps({
    "model": "o3-mini",
    "messages": [
@@ -15,7 +15,7 @@ payload = json.dumps({
          "content": "晚上好"
       }
    ],
-   "max_tokens": 1688,
+   #"max_tokens": 1688,
    "temperature": 0.5,
    "stream": False
 })
@@ -40,3 +40,7 @@ response_json = json.loads(data.decode("utf-8"))
 message_content = response_json['choices'][0]['message']['content']
 print("\n提取的消息内容:")
 print(message_content)
+
+
+#原始响应:
+#{"id":"chatcmpl-CI7CKgO2FsWt365jw3ECPpx3Dlnki","model":"o3-mini-2025-01-31","object":"chat.completion","created":1758433608,"choices":[{"index":0,"message":{"role":"assistant","content":"晚上好！有什么我可以帮您的吗？"},"finish_reason":"stop"}],"system_fingerprint":"fp_e1882df059","usage":{"prompt_tokens":8,"completion_tokens":85,"total_tokens":93,"prompt_tokens_details":{"audio_tokens":0,"cached_tokens":0},"completion_tokens_details":{"accepted_prediction_tokens":0,"audio_tokens":0,"reasoning_tokens":64,"rejected_prediction_tokens":0}}}
